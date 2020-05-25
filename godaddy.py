af3cbe19c970d4ae7b991a5a7cc036209791017f
Tuyen Dang <tuyendq@gmail.com>
2020-05-25 16:36:49 +0700

    add update_record_info function

--- godaddy.py
+++ godaddy.py
@@ -57,13 +57,38 @@ def get_record_info(domain_name, record_type, record_name):
         print(ConnectionError)
         return None
 
+def update_record_info(domain_name, record_type, record_name, ip_address):
+    """Update IP Address of a specific A DNS record
+    /v1/domains/{domain}/records/{type}/{name}
+    Replace all DNS Records for the specified Domain with the specified Type and Name
+    """
+    api_url = API_URL + '/v1/domains/' + domain_name + '/records/' + record_type \
+                        + '/' + record_name
+    headers = {'Content-Type': 'application/json',
+                'Authorization': 'sso-key ' + API_KEY + ':' + API_SECRET}
+    data = [{'data': ip_address, 'ttl': 1800}]
+    try:
+        response = requests.put(api_url, headers=headers, json=data)
+        print("Response status code: " + str(response.status_code))
+        if response.status_code == 200:
+            # print(json.loads(response.content.decode('utf-8')))
+            print(type(response))
+            return response
+        else:
+            print("Check your code, cannot update ip address of dns record")
+            return None
+    except requests.ConnectionError as ConnectionError:
+        print(ConnectionError)
+        return None
+
 def main():
     # Check public ip address
-    check_public_ip()
+    current_public_ip = check_public_ip()
     # Get domain info
     # get_domain_info('ipractice.site')
     # Get dns record
     get_record_info('ipractice.site','A','adfs')
+    update_record_info('ipractice.site','A','129', current_public_ip)
 
 if __name__ == "__main__":
     main()
