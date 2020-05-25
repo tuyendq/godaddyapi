bd350d2577dcf966098350ef1990d77848700752
Tuyen Dang <tuyendq@gmail.com>
2020-05-25 14:10:02 +0700

    add get_record_info function

--- godaddy.py
+++ godaddy.py
@@ -2,7 +2,13 @@ import requests
 import json
 from decouple import config
 
+# Common environment varibles
+API_KEY = config('GODADDY_API_KEY')
+API_SECRET = config('GODADDY_API_SECRET')
+API_URL = config('API_URL')
+
 def check_public_ip():
+    """Check public IP Address using api.ipify.org"""
     try:
         ip = requests.get('https://api.ipify.org').text
         print(ip)
@@ -13,10 +19,6 @@ def check_public_ip():
 
 def get_domain_info(domain_name):
     """Get info of a specific domain"""
-    API_KEY = config('GODADDY_API_KEY')
-    API_SECRET = config('GODADDY_API_SECRET')
-    API_URL = config('API_URL')
-
     api_url = API_URL + '/v1/domains/' + domain_name
     headers = {'Content-Type': 'application/json',
                 'Authorization': 'sso-key ' + API_KEY + ':' + API_SECRET}
@@ -33,11 +35,35 @@ def get_domain_info(domain_name):
         print(ConnectionError)
         return None
 
+def get_record_info(domain_name, record_type, record_name):
+    """Retrieve DNS Records for the specified Domain, 
+    optionally with the specified Type and/or Name
+    url: /v1/domains/{domain}/records/{type}/{name}
+    """
+    api_url = API_URL + '/v1/domains/' + domain_name + '/records/' + record_type \
+                        + '/' + record_name
+    headers = {'Content-Type': 'application/json',
+                'Authorization': 'sso-key ' + API_KEY + ':' + API_SECRET}
+    try:
+        response = requests.get(api_url, headers=headers)
+        print(response.status_code)
+        if response.status_code == 200:
+            print(json.loads(response.content.decode('utf-8')))
+            return json.loads(response.content.decode('utf-8'))
+        else:
+            print("Check your code, cannot get dns record")
+            return None
+    except requests.ConnectionError as ConnectionError:
+        print(ConnectionError)
+        return None
+
 def main():
     # Check public ip address
     check_public_ip()
     # Get domain info
-    get_domain_info('ipractice.site')
+    # get_domain_info('ipractice.site')
+    # Get dns record
+    get_record_info('ipractice.site','A','adfs')
 
 if __name__ == "__main__":
     main()
